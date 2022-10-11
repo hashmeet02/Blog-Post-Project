65a0a975c59f9441e83ee1fafd43735f52110db0
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2022-10-11 00:29:51 -0400

    Assignment 3

--- blog-service.js
+++ blog-service.js
@@ -1,7 +1,9 @@
-const { resolve } = require("path");
+const e = require('express');
 const fs = require("fs");
+
 let posts=[]; 
 let categories=[];
+
 module.exports.initialize=function(){
     return new Promise((resolve,reject)=>{
         var flag1=true;
@@ -35,18 +37,18 @@ module.exports.getAllPosts=function(){
         else{
             resolve(posts);
         }
-    });
+    })
 }
 module.exports.getPublishedPosts=function(){
     return new Promise((resolve,reject)=>{
-        let publishedPosts=posts.filter(post=>post.published===true);
+        let publishedPosts=posts.filter((post)=>post.published===true);
         if(publishedPosts.length==0){
             reject("no results returned");
         }
         else{
             resolve(publishedPosts);
         }
-    });
+    })
 }
 module.exports.getCategories=function(){
     return new Promise((resolve,reject)=>{
@@ -58,3 +60,79 @@ module.exports.getCategories=function(){
         }
     });
 }
+
+module.exports.addPost=function (postData){
+    return new Promise(function(resolve,reject){
+        // if(postData.published==undefined){
+        //     postData.published=false;
+        // }
+        postData.id=posts.length +1;
+        postData.published=(postData.published)? true:false;
+        posts.push(postData);
+        resolve();
+        console.log("added post");
+    });
+}
+
+module.exports.getPostsByCategory=function(category){
+    return new Promise((resolve,reject)=>{
+        let catPosts=posts.filter((post)=>post.category==category);
+        if(catPosts.length==0){
+            reject("no results returned");
+        }
+        else{
+            resolve(catPosts);
+        }
+    })
+}
+
+module.exports.getPostsByCategory=function(category){
+    return new Promise((resolve,reject)=>{
+        let catPosts=posts.filter((post)=>post.category==category);
+        if(catPosts.length==0){
+            reject("no results returned");
+        }
+        else{
+            resolve(catPosts);
+        }
+    })
+}
+
+module.exports.getPostsByMinDate=function(minDateStr){
+    var minDateArr =[];
+    return new Promise(function(resolve, reject){
+        for(var i=0; i<posts.length; i++){
+           if(new Date(posts[i].postDate) >=new Date (minDateStr)){
+               minDateArr.push(posts[i]);
+           }   
+        }
+        if(minDateArr.length ==0){
+            reject("no result returned");
+        }else{
+            resolve(minDateArr);
+        }        
+    });
+}
+
+module.exports.getPostById=function(id){
+    return new Promise((resolve,reject)=>{
+        let postById=posts.filter((post)=>post.id==id);
+        if(postById.length==0){
+            reject("no results returned");
+        }
+        else{
+            resolve(postById[0]);
+        }
+    })
+}
+// module.exports.getPostsByMinDate=function(minDate){
+//     return new Promise((resolve,reject)=>{
+//         let minDatePosts=posts.filter((post)=>post.category==category);
+//         if(publishedPosts.length==0){
+//             reject("no results returned");
+//         }
+//         else{
+//             resolve(publishedPosts);
+//         }
+//     })
+// }
--- server.js
+++ server.js
@@ -3,64 +3,138 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Hashmeet Singh Saini    Student ID: 153070214       Date: 26 Septemeber 2022
+*  Name: Hashmeet Singh Saini    Student ID: 153070214       Date: 10 October 2022
 *
-*  Online (Cyclic) Link: https://hungry-pleat-seal.cyclic.app/about
+*  Online (Cyclic) Link: 
 *
 ********************************************************************************/ 
-var blog_service=require("./blog-service")
-var express=require("express");
 
-var path=require("path")
-var app= express();
-app.use(express.static("public"));
+var blog_service = require("./blog-service");
+var express = require("express");
+const path = require("path");
+var app = express();
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
 
-var HTTP_PORT= process.env.PORT||8080
-
-function onHTTPStart(){
-    console.log("Express http server listening on "+ HTTP_PORT)
-}
+var HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-app.get("/", function(req,res){
+cloudinary.config({
+    cloud_name: 'dr5xws2hc',
+    api_key: '496944954185854',
+    api_secret: 'BwOUoBZEdsn8LAQtAWB8rXr06H0',
+    secure: true
+});
+
+const upload = multer();
+
+function onHttpStart() {
+	console.log(`Express http server listening on: ${HTTP_PORT}`);
+}
+
+app.get("/", (req, res) => {
     res.redirect('/about');
 });
 
-app.get("/about", function(req,res){
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
 });
 
-app.get("/blog", function(req,res){
-    blog_service.getPublishedPosts().then((data)=>{
+app.get("/blog", (req, res) => {
+    blog_service.getPublishedPosts().then((data) => {
         res.json(data);
-    }).catch(function(err){
-        console.log("message: "+err);
+    }).catch((err) => {
+        res.json({message: err});
+    })
+});
+
+app.get("/posts/add", (req, res) => {
+    res.sendFile(path.join(__dirname , '/views/addPost.html'));
+});
+
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+    
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+    
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+    
+    upload(req).then((uploaded)=>{
+        req.body.featureImage = uploaded.url;
+    
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+        blog_service.addPost(req.body).then(() => {
+            res.redirect("/posts");
+        })
+    
+    });
+    
+});
+
+app.get("/posts", (req, res) => {
+    if (req.query.category){
+        blog_service.getPostsByCategory(req.query.category).then((data)=>{
+            res.json(data);
+        }).catch(function(err){
+            res.json({message: err});
+        })
+    }else if(req.query.minDate){
+        blog_service.getPostsByMinDate(req.query.minDate).then(data=>{
+            res.json(data);
+        }).catch(function(err){
+            res.json({message: err});
+        })
+    }else{
+        blog_service.getAllPosts().then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({message:err});
+        });
+    }
+
+});
+
+app.get('/post/:value', (req, res)=>{
+    blog_service.getPostById(req.params.value).then(data=>{
+        res.json(data);
+    }).catch((err)=>{ 
+        res.json( {message: err});
     });
 });
 
-app.get("/posts", function(req,res){
-    blog_service.getAllPosts().then((data)=>{
+app.get("/categories", (req, res) => {
+    blog_service.getCategories().then((data) => {
         res.json(data);
-    }).catch(function(err){
-        console.log("message: "+err);
-    });   
-});
-
-app.get("/categories", function(req,res){
-    blog_service.getCategories().then((data)=>{
-        res.json(data);
-    }).catch(function(err){
-        console.log("message: "+err);
-    }); 
+    }).catch((err) => {
+        res.json({message: err});
+    })
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname,"/views/404page.html"));
-});
+    res.status(404).sendFile(path.join(__dirname, '/views/404page.html'));
+})
 
-blog_service.initialize().then(function(){
-    app.listen(HTTP_PORT, onHTTPStart);
-}).catch(function(err){
-    console.log('Unable to start the server: '+err);
+
+blog_service.initialize().then(function() {
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch(function(err) {
+    console.log(`Unable to start server: ${err}`);
 });
